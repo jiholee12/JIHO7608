6c1682f78feb0f26dd5fda2029b4b98747c4cd59
Lee ji ho <xhdajfr@naver.com>
2023-12-08 03:17:54 +0900

    스크립 (16)

---
+++ script.js
@@ -0,0 +1,14 @@
+let runninTotal =0;
+let buffer ="0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
