152121e376a89a8149c5894fef8307669af0fabf
Lee ji ho <xhdajfr@naver.com>
2023-12-08 03:18:45 +0900

    스크립 (17)

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-let runninTotal =0;
-let buffer ="0";
-let previousOperator;
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runninTotal = 0;
+            break;
 
-const screen = document.querySelector('.screen');
+            case '=':
+                if(previousOperator === null){
+                    return
+                }
+                flushOperation(parseInt(buffer));
+                previousOperator = null;
+                buffer = runninTotal;
+                runninTotal = 0;
+                break;
 
-function buttonClick(value){
-    if(isNaN(value)){
-        handleSymbol(value);
-    }else{
-        handleNumber(value);
+                case'←':
+                if(buffer.length ===1){
+                    buffer ='0';
+                }else{
+                    buffer = buffer.substring(0,buffer.length -1);
+                }
+                break; 
+
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    handleMath(symbol);
+                    break;
     }
-    screen.innerText = buffer;
+}
+
+function handleMath(symbol){
+    if(buffer ==='0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runninTotal === 0){
+        runninTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0'
 }
