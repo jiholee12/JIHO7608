9d7a77b8f94933b5819cb301669823b1eae82042
leejiho <118652753+jiholee12@users.noreply.github.com>
2023-12-08 12:48:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let runninTotal =0;
+let runningTotal = 0;
 let buffer ="0";
 let previousOperator;
 
@@ -17,7 +17,7 @@ function handleSymbol(symbol){
     switch(symbol){
         case 'C':
             buffer = '0';
-            runninTotal = 0;
+            runningTotal = 0;
             break;
 
             case '=':
@@ -26,22 +26,22 @@ function handleSymbol(symbol){
                 }
                 flushOperation(parseInt(buffer));
                 previousOperator = null;
-                buffer = runninTotal;
-                runninTotal = 0;
+                buffer = runningTotal;
+                runningTotal = 0;
                 break;
 
                 case'←':
                 if(buffer.length ===1){
                     buffer ='0';
                 }else{
-                    buffer = buffer.substring(0,buffer.length -1);
+                    buffer = buffer.toString(0,buffer.length -1);
                 }
                 break; 
 
                 case '+':
-                case '-':
-                case '*':
-                case '/':
+                case '−':
+                case '×':
+                case '÷':
                     handleMath(symbol);
                     break;
     }
@@ -54,24 +54,24 @@ function handleMath(symbol){
 
     const intBuffer = parseInt(buffer);
 
-    if(runninTotal === 0){
-        runninTotal = intBuffer;
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
     }else{
         flushOperation(intBuffer);
     }
     previousOperator = symbol;
-    buffer = '0'
+    buffer = '0';
 }
 
 function flushOperation(intBuffer){
     if(previousOperator === '+'){
-        runninTotal += intBuffer;
-    }else if(previousOperator === '-'){
-        runninTotal -= intBuffer;
-    }else if(previousOperator === '*'){
-        runninTotal *= intBuffer
-    }else if(previousOperator === '/'){
-        runninTotal /= intBuffer;
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
     }
 }
 
@@ -84,7 +84,8 @@ function handleNumber(numberString){
 }
 
 function init(){
-    document.querySelector(' .calc-buttons').addEventListener('click',function(event){
+    document.querySelector(' .calc-buttons').
+    addEventListener('click',function(event){
         buttonClick(event.target.innerText);
     })
 }
